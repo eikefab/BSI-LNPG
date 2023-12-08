7fd87a504c1a2688bff892e728ba46b896cfc6dc
eikefab <fabricio.eike.s@gmail.com>
2023-12-08 04:06:29 -0300

    feat: column sort

--- 06-12-2023/src/controller.py
+++ 06-12-2023/src/controller.py
@@ -6,6 +6,9 @@ def create_album(album: str, release_year: int, author: str, debut: bool):
 def list_albums():
     return db.index()
 
+def list_albums_sorted(by: int = 0, descending: bool = False):
+    return sorted(list_albums(), key=lambda item: item[by], reverse=descending)
+
 def get_albums_by_author(author: str):
     pass
 
--- 06-12-2023/src/menus/list_album.py
+++ 06-12-2023/src/menus/list_album.py
@@ -1,5 +1,5 @@
-from tkinter import Tk, Toplevel
-from tkinter.ttk import Frame, Label
+from tkinter import Tk, Toplevel, IntVar
+from tkinter.ttk import Frame, Label, Radiobutton, Checkbutton
 
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -18,15 +18,27 @@ def open(root: Tk):
     build(window, frame)
 
 def build(parent: Toplevel, window: Frame):
-    data = controller.list_albums()
+    by = IntVar(value = 0)
+    descending = IntVar(value=0)
 
     columns = ['Álbum', 'Ano de lançamento', 'Autor', 'Álbum de lançamento']
+    table_widgets = fill(window)
+
+    def column_order():
+        fill(window=window, widgets=table_widgets, data=controller.list_albums_sorted(by.get(), descending=(descending.get() == 1)))
+
+    descending_button = Checkbutton(window, text="Decrescente", variable=descending, command=column_order)
+    descending_button.grid(column=4, row=0, pady=(0, 20))
 
     for table_column in range(len(columns)):
         column = columns[table_column]
 
-        label = Label(window, text=column)
-        label.grid(column=table_column, row=0, padx=(0, 5), pady=(0, 20))
+        radiobutton = Radiobutton(window, text=column, variable=by, value=table_column, command=column_order)
+        radiobutton.grid(column=table_column, row=0, padx=(0, 5), pady=(0, 20))
+
+def fill(window: Toplevel, data: [] = controller.list_albums_sorted(), widgets: [] = []):
+    for widget in widgets:
+        widget.destroy()
 
     for row in range(len(data)):
         info = data[row]
@@ -42,4 +54,6 @@ def build(parent: Toplevel, window: Frame):
             label = Label(window, text=column_data)
             label.grid(column=column, row=(row + 1), padx=(0, 5), pady=(0, 5))
 
+            widgets.append(label)
     
+    return widgets
